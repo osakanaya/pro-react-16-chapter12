96815e0a9f18cc78d9c4433ccf429136251505aa
sashida <sashida@primagest.co.jp>
2020-09-16 22:53:54 +0900

    p.318

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "Ready"
+    };
+  }
+
+  render() {
+    return (
+      <div className="m-2">
+        <div className="h4 bg-primary text-white text-center p-2">
+          { this.state.message }
+        </div>
+        <div className="text-center">
+          <button className="btn btn-primary">
+            Click Me
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
